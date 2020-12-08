473668888cd0b13c21ea589fbe7b3c75ac7e2c00
Stephan Vebrian <stephanvebrian@gmail.com>
2020-12-08 23:23:55 +0700

    user migration & env.exampe update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            // $table->string('gender');
+            $table->enum('gender', ['male', 'female', 'other']);
             $table->rememberToken();
             $table->timestamps();
         });
