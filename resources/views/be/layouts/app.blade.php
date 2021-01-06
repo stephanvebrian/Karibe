307c2a11024ed5d780c0253bce9ecb36579511b0
Stephan Vebrian <stephanvebrian@gmail.com>
2021-01-07 01:33:53 +0700

    fix nav index

--- resources/views/be/layouts/app.blade.php
+++ resources/views/be/layouts/app.blade.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>Index</title>
+    <title>Admin Panel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Font Awesome -->
