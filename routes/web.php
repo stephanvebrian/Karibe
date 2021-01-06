b19a02d6cd6111b6af83fdc5ea52354275268842
Stephan Vebrian <stephanvebrian@gmail.com>
2021-01-06 13:01:57 +0700

    add middleware create post

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,21 @@ Route::get('/', function () {
 });
 
 Route::get('/home', 'Frontend\HomeController@home')->name('fe.home');
-Route::get('/post/create', 'Frontend\PostController@create')->name('fe.post.create');
-Route::post('/post/store', 'Frontend\PostController@store')->name('fe.post.store');
 Route::get('/post/{slug}', 'Frontend\PostController@show')->name('fe.post.show');
 
 Route::group(['middleware' => ['roles:Member']], function () {
-
+    
 });
 
 Route::group(['middleware' => ['roles:Admin']], function () {
     
 });
 
+Route::group(['middleware' => ['roles:Member,Admin']], function () {
+    Route::get('/post/create', 'Frontend\PostController@create')->name('fe.post.create');
+    Route::post('/post/store', 'Frontend\PostController@store')->name('fe.post.store');
+    
+});
+
 Auth::routes([ 'reset' => true, 'confirm' => false ]);
-// Route::get('/home', 'HomeController@index')->name('home');
 
